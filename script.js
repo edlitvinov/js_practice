7b912e1fd83fc8ebc89ba5b08605d679f400537c
ED <edoss@list.ru>
2021-03-30 23:48:39 +0400

    example_2

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-"use strict";
+ "use strict";
+ const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ' ');
+ const personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actors: {},
+     genres: [],
+     privat: false
+ };
 
-// let num = 70;
+ //  const a = prompt('Один из последних просмотренных фильмов?', ''),
+ //      b = +prompt('На сколько оцените его?', ''),
+ //      c = prompt('Один из последних просмотренных фильмов?', ''),
+ //      d = +prompt('На сколько оцените его?', '');
 
-// while (num <= 75) {
-//     console.log(num);
-//     num++;
-// }
+ //  personalMovieDB.movies[a] = b;
+ //  personalMovieDB.movies[c] = d;
 
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num <= 75);
+ for (let i = 0; i < 2; i++) {
+     const a = prompt('Один из последних просмотренных фильмов?', ''),
+         b = +prompt('На сколько оцените его?', '');
+     if (a != null && b != null && a != '' && b != null && a.length < 50) {
+         personalMovieDB.movies[a] = b;
+         console.log('done');
+     } else {
+         console.log('error');
+         i--;
+     }
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        //break; //останавливает цикл
-        continue; // прерывает цикл в точке условия и продолжает его далее
-    }
-    console.log(i);
-}
+ }
+
+ if (personalMovieDB.count < 10) {
+     console.log("Просмотрено довольно мало фильмов");
+
+ } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+     console.log("Вы классический зритель");
+ } else if (personalMovieDB.count > 30) {
+     console.log("Вы киноман");
+ } else {
+     console.log("Произошла ошибка");
+ }
+
+ console.log(personalMovieDB);
