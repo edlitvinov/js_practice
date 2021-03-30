a764ebe41051d27fbc5576442351251568af16b3
ED <edoss@list.ru>
2021-03-30 19:16:59 +0400

    ternary

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 "use strict";
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ' ');
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = +prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = +prompt('На сколько оцените его?', '');
+// if (4 == 7) {
+//     console.log("OK!");
+// } else {
+//     console.log("error!");
+// }
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// const num = 70;
 
-console.log(personalMovieDB);
+// if (num < 37) {
+//     console.log("error!");
+// } else if (num > 90) {
+//     console.log("много");
+// } else {
+//     console.log("OK!");
+// }
+
+// (num === 80) ? console.log("error!"): console.log("OK!");
+
+const num = 50;
+
+switch (num) {
+    case 37:
+        console.log("неверно!");
+        break;
+    case 100:
+        console.log("неверно!");
+        break;
+    case 20:
+        console.log("верно!");
+        break;
+    default:
+        console.log("не в этот раз!");
+        break;
+}
